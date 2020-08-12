17819844cc16e9b9b976f8bfb9c4e4a72a3c2169
felixaserna <soporte1@grupoaspec.com>
2020-08-12 17:31:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
 
     <link rel="stylesheet" href="dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="dist/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
 </head>
 <body>
-
-    
 
     <script src="dist/js/jquery-3.5.1.slim.min.js"></script>
     <script src="dist/js/popper.min.js"></script>
