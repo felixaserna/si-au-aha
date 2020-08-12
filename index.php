31619102dc1876942fec1dd33c7b33d7b384fb8b
felixaserna <soporte1@grupoaspec.com>
2020-08-12 17:26:43 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="dist/fontawesome/css/all.min.css">
+</head>
+<body>
+
+    
+
+    <script src="dist/js/jquery-3.5.1.slim.min.js"></script>
+    <script src="dist/js/popper.min.js"></script>
+    <script src="dist/js/bootstrap.min.js"></script>
+</body>
+</html>
