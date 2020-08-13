e8e720b0d8df40cadee26147f2391952e7d140e6
felixaserna <soporte1@grupoaspec.com>
2020-08-13 17:27:10 -0500

    Commit

--- config/config.php
+++ config/config.php
@@ -1,5 +1,12 @@
-<?php 
+<?php
 
-    
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=si-au-aha', 'root', '');
+        
+        // echo 'Conectado';
+    } catch (PDOException $e) {
+        print "¡Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
 
 ?>
--- panel/admin/index.php
+++ panel/admin/index.php
@@ -1,3 +1,10 @@
+<?php 
+
+    include_once '../../config/config.php';
+
+    $sql = "SELECT * FROM "
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -64,12 +71,22 @@
                     </table>
 
                     <nav aria-label="Page navigation example">
-                        <ul class="pagination">
-                            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                            <li class="page-item"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item">
+                                <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">1</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">2</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">3</a>
+                            </li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">Next</a>
+                            </li>
                         </ul>
                     </nav>
 
