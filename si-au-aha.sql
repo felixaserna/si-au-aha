4f1e78e1629b5fd56cebcbf9dfeee41ecb2040e4
felixaserna <soporte1@grupoaspec.com>
2020-08-24 19:29:23 -0500

    Updated

--- si-au-aha.sql
+++ si-au-aha.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-08-2020 a las 03:00:47
+-- Tiempo de generación: 14-08-2020 a las 03:23:56
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 7.2.12
 
@@ -30,15 +30,25 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `registro_facturas` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `apellidoPaterno` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `apellidoMaterno` varchar(255) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-  `sede` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `nombre` text COLLATE utf8_unicode_ci NOT NULL,
+  `apellidoPaterno` text COLLATE utf8_unicode_ci NOT NULL,
+  `apellidoMaterno` text COLLATE utf8_unicode_ci NOT NULL,
+  `sede` text COLLATE utf8_unicode_ci NOT NULL,
   `fechaCurso` date NOT NULL,
-  `factura` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `proveedor` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `proveedor` text COLLATE utf8_unicode_ci NOT NULL,
   `fechaCompra` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `registro_facturas`
+--
+
+INSERT INTO `registro_facturas` (`id`, `nombre`, `apellidoPaterno`, `apellidoMaterno`, `sede`, `fechaCurso`, `proveedor`, `fechaCompra`) VALUES
+(1, 'MARIA', 'CORDOVA', 'MIÃ‘ON', 'CCCAMP Xalapa', '2019-11-30', 'WorlPoin', '2019-11-07'),
+(2, 'MAGALI ELIZABETH', 'ROSAS', 'PACHECO', 'CCCAMP Xalapa', '2019-11-03', 'WorlPoin', '2019-11-07'),
+(3, 'CIRENIA GUADALUPE', 'FLORES', 'RUIZ', 'CCCAMP Xalapa', '2019-11-30', 'WorlPoin', '2019-11-07'),
+(4, 'MARIA DE LOURDES', 'CORDOBA', 'MAVIL', 'CCCAMP Xalapa', '2019-11-30', 'WorlPoin', '2019-11-07'),
+(5, 'CESAR ALEJANDRO', 'MARTINEZ', 'LOPEZ', 'CCCAMP Xalapa', '2019-11-30', 'WorlPoin', '2019-11-07');
 
 --
 -- Índices para tablas volcadas
@@ -58,7 +68,7 @@ ALTER TABLE `registro_facturas`
 -- AUTO_INCREMENT de la tabla `registro_facturas`
 --
 ALTER TABLE `registro_facturas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
